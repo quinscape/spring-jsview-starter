0bba17b72f7aa69a0917bf8b91687e7d9dba8ce1
Sven Helmberger <fforw@gmx.de>
2018-09-07 00:25:57 +0200

    Add Spring Initializr Web Project
    Generate a new Spring boot artifact with http://start.spring.io/

---
+++ src/main/java/de/quinscape/springjsviewstarter/SpringJsviewStarterApplication.java
@@ -0,0 +1,12 @@
+package de.quinscape.springjsviewstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJsviewStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJsviewStarterApplication.class, args);
+	}
+}
---
+++ src/test/java/de/quinscape/springjsviewstarter/SpringJsviewStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package de.quinscape.springjsviewstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJsviewStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
