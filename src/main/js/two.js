08c3d4f3151f49a0ef8af1bfa0fc703ba48c1191
Sven Helmberger <fforw@gmx.de>
2018-09-07 12:17:12 +0200

    Add simple style
    Adds simple example style to show how to include component CSS with our
    webpack setup.

--- src/main/js/two.js
+++ src/main/js/two.js
@@ -1,6 +1,9 @@
 import bootstrap from "jsview-bootstrap"
 import React from "react"
 
+// noinspection ES6UnusedImports
+import EXAMPLE_STYLE from "./style.css"
+
 bootstrap(
     initial => (
         <React.Fragment>
